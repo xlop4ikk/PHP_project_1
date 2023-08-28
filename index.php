9fdd00bf5a02b20fb08a66c4ff8961d731f8a4dd
xlop4ikk <bolshakovdan621@mail.ru>
2023-08-28 14:18:15 +0500

    Some fixes

--- index.php
+++ index.php
@@ -14,19 +14,40 @@
 
     # Проверка на правильное соединение
     if (!$dbconn) {
-    die('Could not connect');
+        die("Could not connect</br>");
     }
     else {
-    echo ("Connected to local DB");
+        echo ("Connected to local DB</br>");
     }
 
-    $query = "INSERT INTO book (book_name, book_author, book_price) VALUES ('$book_name_from_html', '$book_author_from_html', '$book_price_from_html')";
-    $result = pg_query($dbconn, $query);
-    if(!$result) {
-    echo "Could not insert into database";
+    # Добавление данных в бд
+    $query_1 = "INSERT INTO book (book_name, book_author, book_price) VALUES ('$book_name_from_html', '$book_author_from_html', '$book_price_from_html')";
+    if (isset($_POST['input_1'])) {
+        $result_1 = pg_query($dbconn, $query_1);
+        echo "Inserted successfully</br>";
     }
     else {
-    echo "Inserted successfully";
+        echo "Inserting error</br>";
     }
+
+    # Вывод списка на экран
+    $query_2 = 'SELECT * FROM book';
+    if (isset($_POST['input_2'])) {
+        $result_2 = pg_query($dbconn, $query_2);
+        echo "<table>\n";
+        while ($line = pg_fetch_array($result_2, null, PGSQL_ASSOC)) {
+            echo "\t<tr>\n";
+            foreach ($line as $col_value) {
+                echo "\t\t<td>$col_value</td>\n";
+            }
+            echo "\t</tr>\n";
+        }
+        echo "</table>\n";
+    }
+    else {
+        echo "Could not display values</br>";
+    }
+
+    pg_close($dbconn);
 
 ?>
